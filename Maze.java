cc3f7aff2d2ac6cb3d447a68d1f0def36efc876f
Amanda Chen <achen01@stuy.edu>
2019-02-27 23:13:30 -0500

    fixed constructor by initializing maze

--- Maze.java
+++ Maze.java
@@ -18,80 +18,89 @@ public class Maze{
 
       3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then:
          throw a FileNotFoundException or IllegalStateException
-    */
-    public Maze(String filename) throws FileNotFoundException{
-      try{
+  */
+  public Maze(String filename) throws FileNotFoundException{
+    animate = false;
 
-        animate = false;
+    File f = new File(filename);
+    Scanner scan = new Scanner(f);
+    String output = "";
+    int r = 0;
+    int c = 0;
 
-        File f = new File(filename);
-        Scanner scan = new Scanner(f);
-        String output = "";
-        int r = 0;
-        int c = 0;
-
-        while (scan.hasNextLine()){
-          String line = scan.nextLine();
-          c = line.length();
-          output += line;
-          r++;
-        }
-
-        int idx = 0;
-        int countS = 0;
-        int countE = 0;
-
-        for (int i = 0; i < maze.length; i++){
-          for (int j = 0; j < maze[0].length; j++){
-            maze[i][j] = output.charAt(idx);
-            if (maze[i][j] == 'S'){
-              countS++;
-            }
-            if (maze[i][j] == 'E'){
-              countE++;
-            }
-            idx++;
-          }
-        }
-
-        if (countS != 1 || countE != 1){
-          throw new IllegalStateException("Too many/No Start/End");
-        }
-
-      }catch(FileNotFoundException e){
-        System.out.println("File Not Found");
-      }
-
+    while (scan.hasNextLine()){
+      String line = scan.nextLine();
+      c = line.length();
+      output += line;
+      r++;
     }
 
-    public String toString(){
-      String output = "";
-      for (int i = 0; i < maze.length; i++){
-        for (int j = 0; j < maze[0].length; j++){
-          output += maze[i][j];
-          if (j == maze[0].length - 1){
-            output += "\n";
-          }
+     maze = new char[r][c];
+    int idx = 0;
+    int countS = 0;
+    int countE = 0;
+
+    for (int i = 0; i < maze.length; i++){
+      for (int j = 0; j < maze[0].length; j++){
+        maze[i][j] = output.charAt(idx);
+        if (maze[i][j] == 'S'){
+          countS++;
+        }
+        if (maze[i][j] == 'E'){
+          countE++;
+        }
+        idx++;
+      }
+    }
+
+    if (countS != 1 || countE != 1){
+      throw new IllegalStateException("Too many/No Start/End");
+    }
+
+  }
+
+  public String toString(){
+    String output = "";
+    for (int i = 0; i < maze.length; i++){
+      for (int j = 0; j < maze[0].length; j++){
+        output += maze[i][j];
+        if (j == maze[0].length - 1){
+          output += "\n";
         }
       }
-      return output;
     }
+    return output;
+  }
 
-    private void wait(int millis){
-      try {
-        Thread.sleep(millis);
-      }catch (InterruptedException e) {
-        
-      }
-     }
+  private void wait(int millis){
+    try {
+      Thread.sleep(millis);
+    }catch (InterruptedException e) {
 
-    public void setAnimate(boolean b){
-        animate = b;
     }
+   }
 
-    public void clearTerminal(){
-        //erase terminal, go to top left of screen.
-        System.out.println("\033[2J\033[1;1H");
-    }
+  public void setAnimate(boolean b){
+      animate = b;
+  }
+
+  public void clearTerminal(){
+      //erase terminal, go to top left of screen.
+      System.out.println("\033[2J\033[1;1H");
+  }
+
+  /*Wrapper Solve Function returns the helper function
+    Note the helper function has the same name, but different parameters.
+    Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
+  */
+  public int solve(){
+    return 0;
+          //find the location of the S.
+
+          //erase the S
+
+          //and start solving at the location of the s.
+          //return solve(???,???);
+  }
 
 }
